adc8cfd2256cec18d77436e2495b14d01a44683c
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2023-01-27 12:10:03 +0100

    feat: Init-project (#1)

--- app/main.py
+++ app/main.py
@@ -1,27 +1,29 @@
-from ast import If
-import json
 import os
+from typing import Dict, Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> Dict[str, str]:
     return {"root": "ok"}
 
+
 @app.get("/api")
-async def api():
+async def api() -> Dict[str, Optional[str]]:
     my_configmap_value = os.getenv("MY_CONFIGMAP_VALUE")
     my_secret_value = os.getenv("MY_SECRET_VALUE")
 
     api_values = {
-        "my_configmap_value" : my_configmap_value,
-        "my_secret_value" : my_secret_value
+        "my_configmap_value": my_configmap_value,
+        "my_secret_value": my_secret_value,
     }
 
     return api_values
 
+
 @app.get("/status")
-async def status():
+async def status() -> Dict[str, str]:
     return {"status": "ok"}
